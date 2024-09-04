2b546160e092547d8c322eb93e371785d82efe89
Alex Handley <smithvillescholarsah@gmail.com>
2024-09-03 19:32:17 -0500

    wip

--- db-config.js
+++ db-config.js
@@ -1,5 +1,6 @@
 const knex = require("knex");
 const config = require("./knexfile.js");
 const environment = process.env.NODE_ENV || "testing";
+console.log("this thing... =", config);
 
 module.exports = knex(config[environment]);
--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,15 @@
 const settings = {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    migrations: { directory: "./data/migrations" },
-    seeds: { directory: "./data/seeds" }
-  }
-  
-  module.exports = {
-    testing: {
-      ...settings,
-      connection: {
-        filename: "./data/characters.db3",
-      }
-    }
-  };
-  
+  client: "sqlite3",
+  useNullAsDefault: true,
+  migrations: { directory: "./data/migrations" },
+  seeds: { directory: "./data/seeds" },
+};
+
+module.exports = {
+  testing: {
+    ...settings,
+    connection: {
+      filename: "./data/characters.db3",
+    },
+  },
+};
